a9828da0e18fa401305a486d85c88e893cdcf513
Diego Aguilar <67769941+cyuxhtby@users.noreply.github.com>
2025-01-23 16:45:38 -0300

    feat: inherit 4626

--- src/Strategy.sol
+++ src/Strategy.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {BaseStrategy, ERC20} from "@yearn/tokenized-strategy/BaseStrategy.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20, IERC20, SafeERC20, ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IPool} from "@aave/aave-v3-core/contracts/interfaces/IPool.sol";
 import {IAToken} from "@aave/aave-v3-core/contracts/interfaces/IAToken.sol";
 
-
-contract Strategy is BaseStrategy {
+contract Strategy is ERC4626 {
     error ZeroValue();
 
     using SafeERC20 for ERC20; // Handles weird erc20s
@@ -16,52 +14,32 @@ contract Strategy is BaseStrategy {
     IPool public immutable aaveV3Pool;
     IAToken public immutable aToken;
 
-
-    // The only default global variables from the BaseStrategy that can be accessed from storage is `asset` and `TokenizedStrategy`.
-
-    constructor(address _asset, string memory _name, address _pool) BaseStrategy(_asset, _name) {
+    constructor(address _asset, address _pool, string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+        ERC4626(IERC20(_asset))
+    {
         aaveV3Pool = IPool(_pool);
         aToken = IAToken(aaveV3Pool.getReserveData(address(_asset)).aTokenAddress);
     }
 
-    /// @notice Deploy funds into the Aave V3 lending pool
-    /// @dev Called via BaseStrategy.deployFunds() to deploy available funds in the strategy.
-    ///      This happens:
-    ///      - After new deposits (when users call Strategy.deposit() or Strategy.mint() via fallbacks)
-    ///      - During tend() calls if implemented
-    ///      - During report() profit harvesting
-    /// @param _amount The amount of asset to deposit into Aave
-    function _deployFunds(uint256 _amount) internal override {
-        require(_amount > 0, ZeroValue());
-        // supply(asset, amount, onBehalfOf, referralCode)
-        aaveV3Pool.supply(address(asset), _amount, address(this), 0);
+    /// @notice Returns the total amount of value locked in vault.
+    function totalAssets() public view override returns (uint256) {
+        return IERC20(asset()).balanceOf(address(this)) + aToken.balanceOf(address(this));
     }
 
-    /// @notice Withdraw funds from the Aave V3 lending pool
-    /// @dev If _amount is greater than aToken balance, withdraws full available balance 
-    /// @param _amount The amount requested to withdraw
-    function _freeFunds(uint256 _amount) internal override {
-        // withdraw(asset, amount, to)
-        aaveV3Pool.withdraw(
-            address(asset), 
-            Math.min(aToken.balanceOf(address(this)), _amount), 
-            address(this)
-        );
+    /// @notice Deposits assets into the vault and supplies them to aave
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
+        super._deposit(caller, receiver, assets, shares);
+        IERC20(asset()).approve(address(aaveV3Pool), assets);
+        aaveV3Pool.supply(asset(), assets, address(this), 0);
     }
 
-    /// @notice Returns the total assets the strategy has including both deployed and undeployed funds
-    /// @dev aTokens automatically accrue interest so no manual harvesting is needed
-    /// @return _totalAssets Total value of assets denominated in asset token
-    function _harvestAndReport() internal view override returns (uint256 _totalAssets) {
-           return asset.balanceOf(address(this)) + aToken.balanceOf(address(this));
+    /// @notice Withdraws assets from aave and then the vault
+    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
+        internal
+        override
+    {
+        aaveV3Pool.withdraw(asset(), Math.min(aToken.balanceOf(address(this)), assets), address(this));
+        super._withdraw(caller, receiver, owner, assets, shares);
     }
-
-
-    /// @notice Returns how much can be withdrawn immediatly from the strategy
-    /// @dev Aave imposes no withdraw restrictions so we can assume full amount
-    /// @return . Max amount of assets available for withdrawal
-    function availableWithdrawLimit() public view returns (uint256) {
-        return asset.balanceOf(address(this)) + aToken.balanceOf(address(this));
-    }
-
 }
